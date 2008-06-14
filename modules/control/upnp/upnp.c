2fbd7cad52c9b7396ff2a7c55fabd61606b3ebdd
Mirsal Ennaime <mirsal.ennaime@gmail.com>
2008-06-14 16:56:10 +0200

    UPnP Media Server module stub

---
+++ modules/control/upnp/upnp.c
@@ -0,0 +1,85 @@
+/*****************************************************************************
+ * upnp.c : UPnP A/V Media Server
+ *****************************************************************************
+ * Copyright © 2008 Mirsal Ennaime
+ * $Id 
+ *
+ * Authors:     Mirsal Ennaime <mirsal dot ennaime at gmail dot com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ *****************************************************************************/
+
+#include <vlc_common.h>
+#include <vlc_plugin.h>
+#include <vlc_interface.h>
+
+static int  Open    ( vlc_object_t * );
+static void Close   ( vlc_object_t * );
+static void Run     ( intf_thread_t * );
+
+struct intf_sys_t
+{
+};
+
+/*****************************************************************************
+ * Module descriptor
+ *****************************************************************************/
+
+vlc_module_begin();
+    set_shortname( N_("upnp"));
+    set_category( CAT_INTERFACE );
+    set_subcategory( SUBCAT_INTERFACE_CONTROL );
+    set_description( N_("UPnP A/V Media Server") );
+    set_capability( "interface", 0 );
+    set_callbacks( Open, Close );
+vlc_module_end();
+
+/*****************************************************************************
+ * Open: initialize interface
+ *****************************************************************************/
+
+static int Open( vlc_object_t* p_this )
+{
+    intf_thread_t   *p_intf = (intf_thread_t*)p_this;
+    intf_sys_t      *p_sys  = malloc( sizeof( intf_sys_t ) );
+    
+    p_intf->pf_run = Run;
+    p_intf->p_sys = p_sys;
+
+    return VLC_SUCCESS;
+}
+
+/*****************************************************************************
+ * Close: cleanup interface
+ *****************************************************************************/
+
+static void Close( vlc_object_t *p_this )
+{
+    intf_thread_t   *p_intf     = (intf_thread_t*) p_this;
+    
+    free( p_intf->p_sys );
+}
+
+/*****************************************************************************
+ * Run: main loop
+ *****************************************************************************/
+
+static void Run( intf_thread_t *p_intf )
+{
+    while( !intf_ShouldDie( p_intf ) )
+    {
+        msleep( INTF_IDLE_SLEEP );
+    }
+}
