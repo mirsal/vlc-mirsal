bfd535181b65fd52fbccd23f6dd6abd5249a6512
Mirsal Ennaime <mirsal.ennaime@gmail.com>
2010-08-15 04:22:54 +0200

    dbus: Add a proof-of-concept implementation of the Raise method

--- modules/control/dbus/dbus_root.c
+++ modules/control/dbus/dbus_root.c
@@ -31,6 +31,9 @@
 #include <vlc_common.h>
 #include <vlc_interface.h>
 
+#include <xcb/xcb.h>
+#include <xcb/xcb_icccm.h>
+
 #include "dbus_root.h"
 #include "dbus_common.h"
 
@@ -178,6 +181,101 @@ DBUS_METHOD( Quit )
     REPLY_SEND;
 }
 
+#define WM_WINDOW_ROLE_S "WM_WINDOW_ROLE"
+#define VLC_MAIN_WINDOW_ROLE "vlc-main"
+
+DBUS_METHOD( Raise )
+{
+/*  TODO: Make this piece of code more concise.
+    TODO: Use ifdefs to include this code only when VLC is built with X support.
+    TODO: Correctly detect the current screen instead of using the first one.
+    TODO: Use _NET_WM_PID to raise only this VLC instance's main window.
+    TODO: Use _NET_ACTIVE_WINDOW so the window is raised even when it is
+          withdrawn, minimized and/or not on the current viewport.
+*/
+
+    VLC_UNUSED( p_this );
+    REPLY_INIT;
+
+    xcb_connection_t        *p_x_conn   = xcb_connect( NULL, NULL );
+    const xcb_setup_t       *p_x_setup  = xcb_get_setup( p_x_conn );
+    xcb_screen_iterator_t    roots      = xcb_setup_roots_iterator( p_x_setup );
+    xcb_screen_t            *p_x_screen = roots.data; /* XXX */
+    xcb_intern_atom_reply_t *p_intern_atom = NULL;
+    xcb_atom_t               wm_window_role;
+
+    p_intern_atom = xcb_intern_atom_reply ( p_x_conn,
+        xcb_intern_atom( p_x_conn, 0,
+                         strlen( WM_WINDOW_ROLE_S ),
+                         WM_WINDOW_ROLE_S ), NULL );
+
+    if( !p_intern_atom )
+        return DBUS_HANDLER_RESULT_NEED_MEMORY;
+
+    wm_window_role = p_intern_atom->atom;
+    free( p_intern_atom );
+
+    xcb_query_tree_reply_t *p_tree;
+    p_tree = xcb_query_tree_reply( p_x_conn,
+                                   xcb_query_tree( p_x_conn, p_x_screen->root ),
+                                   NULL );
+
+    if( !p_tree )
+        return DBUS_HANDLER_RESULT_NEED_MEMORY;
+
+    for( int i = 0; i < xcb_query_tree_children_length( p_tree ); i++ )
+    {
+        size_t i_len = 0, i_offset = (size_t) i * sizeof( xcb_window_t );
+        char* psz_wm_window_role = NULL;
+        xcb_window_t window;
+        xcb_get_property_cookie_t cookie;
+        xcb_get_property_reply_t *p_reply;
+
+        window  = *(xcb_query_tree_children( p_tree ) + i_offset);
+        cookie  = xcb_get_text_property( p_x_conn, window, wm_window_role );
+        p_reply = xcb_get_property_reply( p_x_conn, cookie, NULL );
+
+        if( !p_reply ||
+             0 == ( i_len = xcb_get_property_value_length( p_reply ) ) )
+        {
+            free( p_reply );
+            p_reply = NULL;
+            continue;
+        }
+
+        psz_wm_window_role = calloc( i_len + 1, sizeof( char ) );
+        strncpy( psz_wm_window_role, xcb_get_property_value( p_reply ), i_len );
+        free( p_reply );
+
+        if( !psz_wm_window_role ||
+             strncmp( psz_wm_window_role, VLC_MAIN_WINDOW_ROLE, i_len ) )
+        {
+            free( psz_wm_window_role );
+            psz_wm_window_role = NULL;
+            continue;
+        }
+
+        msg_Dbg( (vlc_object_t*) p_this,
+                 "Found a VLC main window, raising it" );
+
+        /* XXX */
+        const uint32_t p_stack_mode[] = { XCB_STACK_MODE_ABOVE };
+        xcb_map_window( p_x_conn, window );
+        xcb_configure_window( p_x_conn, window,
+                              XCB_CONFIG_WINDOW_STACK_MODE,
+                              p_stack_mode );
+
+        free( psz_wm_window_role );
+        psz_wm_window_role = NULL;
+    }
+
+    free( p_tree );
+    REPLY_SEND;
+}
+
+#undef WM_WINDOW_ROLE_S
+#undef VLC_MAIN_WINDOW_ROLE
+
 DBUS_METHOD( handle_introspect_root )
 { /* handles introspection of root object */
     VLC_UNUSED(p_this);
@@ -243,6 +341,7 @@ handle_root ( DBusConnection *p_conn, DBusMessage *p_from, void *p_this )
         return handle_introspect_root( p_conn, p_from, p_this );
 
     METHOD_FUNC( DBUS_INTERFACE_PROPERTIES, "Get",          GetProperty );
+    METHOD_FUNC( DBUS_MPRIS_ROOT_INTERFACE, "Raise",        Raise );
     METHOD_FUNC( DBUS_MPRIS_ROOT_INTERFACE, "Quit",         Quit );
 
     return DBUS_HANDLER_RESULT_NOT_YET_HANDLED;
