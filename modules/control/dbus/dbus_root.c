4d0c3e363952c63b6989208cde627cc6ffb4c9b7
Mirsal Ennaime <mirsal@mirsal.fr>
2011-05-15 12:04:25 +0200

    dbus: Make the Raise method recursively look for a main X window in the list of window which have the correct _NET_WM_PID hint set
    :/!\: This piece of code is still defunct and has memleaks :/!\:

--- modules/control/dbus/dbus_root.c
+++ modules/control/dbus/dbus_root.c
@@ -34,6 +34,9 @@
 #include <xcb/xcb.h>
 #include <xcb/xcb_icccm.h>
 
+#include <unistd.h>
+#include <limits.h>
+
 #include "dbus_root.h"
 #include "dbus_common.h"
 
@@ -188,28 +191,24 @@ DBUS_METHOD( Quit )
     REPLY_SEND;
 }
 
-#define WM_WINDOW_ROLE_S "WM_WINDOW_ROLE"
-#define VLC_MAIN_WINDOW_ROLE "vlc-main"
+#define WM_WINDOW_ROLE_S "WM_CLASS"
+#define _NET_WM_PID_S "_NET_WM_PID"
+#define VLC_MAIN_WINDOW_ROLE "vlc"
 
-DBUS_METHOD( Raise )
+static int find_vlc_main_window( intf_thread_t    *p_this,
+                                 xcb_connection_t *p_x_conn,
+                                 xcb_window_t      parent,
+                                 xcb_window_t     *vlc_main_window )
 {
-/*  TODO: Make this piece of code more concise.
-    TODO: Use ifdefs to include this code only when VLC is built with X support.
-    TODO: Correctly detect the current screen instead of using the first one.
-    TODO: Use _NET_WM_PID to raise only this VLC instance's main window.
-    TODO: Use _NET_ACTIVE_WINDOW so the window is raised even when it is
-          withdrawn, minimized and/or not on the current viewport.
-*/
+    xcb_window_t            *p_children  = NULL;
+    xcb_generic_error_t     *p_x_error   = NULL;
+    xcb_query_tree_reply_t  *p_tree      = NULL;
+    size_t                   i_tree_len  = 0;
 
-    VLC_UNUSED( p_this );
-    REPLY_INIT;
-
-    xcb_connection_t        *p_x_conn   = xcb_connect( NULL, NULL );
-    const xcb_setup_t       *p_x_setup  = xcb_get_setup( p_x_conn );
-    xcb_screen_iterator_t    roots      = xcb_setup_roots_iterator( p_x_setup );
-    xcb_screen_t            *p_x_screen = roots.data; /* XXX */
     xcb_intern_atom_reply_t *p_intern_atom = NULL;
     xcb_atom_t               wm_window_role;
+
+    xcb_get_property_cookie_t *p_cookies = NULL;
 
     p_intern_atom = xcb_intern_atom_reply ( p_x_conn,
         xcb_intern_atom( p_x_conn, 0,
@@ -217,9 +216,137 @@ DBUS_METHOD( Raise )
                          WM_WINDOW_ROLE_S ), NULL );
 
     if( !p_intern_atom )
-        return DBUS_HANDLER_RESULT_NEED_MEMORY;
+        return VLC_ENOMEM;
 
     wm_window_role = p_intern_atom->atom;
+    free( p_intern_atom );
+
+    p_tree = xcb_query_tree_reply( p_x_conn,
+                                   xcb_query_tree( p_x_conn, parent ),
+                                   &p_x_error );
+
+    if( !p_tree )
+        return VLC_ENOMEM;
+
+    i_tree_len = xcb_query_tree_children_length( p_tree );
+    p_children = xcb_query_tree_children( p_tree );
+    p_cookies  = calloc( i_tree_len + 1, sizeof( xcb_get_property_cookie_t ) );
+
+    msg_Dbg( (vlc_object_t*) p_this, "This window has %d children", i_tree_len );
+
+    for( int i = 1; (size_t) i <= i_tree_len; i++ )
+    {
+        xcb_window_t vlc_window;
+        if( VLC_SUCCESS == find_vlc_main_window( p_this,
+                                                 p_x_conn,
+                                                 p_children[i],
+                                                 &vlc_window ) )
+        {
+            *vlc_main_window = vlc_window;
+            return VLC_SUCCESS;
+        }
+
+        p_cookies[i] = xcb_get_text_property( p_x_conn,
+                                              p_children[i],
+                                              wm_window_role );
+    }
+
+    p_cookies[0] = xcb_get_text_property( p_x_conn,
+                                          parent,
+                                          wm_window_role );
+
+    for( int i = 0; (size_t) i <= i_tree_len; i++ )
+    {
+        int i_len = 0;
+        char* psz_wm_window_role = NULL;
+        xcb_get_property_reply_t *p_reply = NULL;
+
+        p_reply = xcb_get_property_reply( p_x_conn, p_cookies[i], &p_x_error );
+
+        if( !p_reply ||
+             0 == ( i_len = xcb_get_property_value_length( p_reply ) ) )
+        {
+            if( !p_reply )
+                msg_Err( (vlc_object_t*) p_this,
+                         "No reply: iteration: %d Error code %d; %s:%d",
+                         i, p_x_error->error_code, __FILE__, __LINE__ );
+            else
+                msg_Err( (vlc_object_t*) p_this,
+                         "Empty X reply: iteration: %d; %s:%d",
+                         i, __FILE__, __LINE__ );
+
+            free( p_reply );
+            free( p_x_error );
+            p_reply = NULL;
+            continue;
+        }
+
+        psz_wm_window_role = calloc( i_len + 1, sizeof( char ) );
+        strncpy( psz_wm_window_role, xcb_get_property_value( p_reply ), i_len );
+        free( p_reply );
+
+        msg_Dbg( (vlc_object_t*) p_this, "WM_WINDOW_ROLE = %s",
+                                         psz_wm_window_role );
+        if( psz_wm_window_role &&
+            !strncmp( psz_wm_window_role, VLC_MAIN_WINDOW_ROLE, i_len ) )
+        {
+            free( psz_wm_window_role );
+            *vlc_main_window = p_children[i];
+            return VLC_SUCCESS;
+        }
+
+        free( psz_wm_window_role );
+    }
+
+    return VLC_EGENERIC;
+}
+
+DBUS_METHOD( Raise )
+{
+/*  TODO: Make this piece of code more concise.
+    TODO: Use ifdefs to include this code only when VLC is built with X support.
+    TODO: Use _NET_ACTIVE_WINDOW so the window is raised even when it is
+          withdrawn, minimized and/or not on the current viewport.
+*/
+
+    REPLY_INIT;
+
+    int i_x_screen = -1, i_tree_len = 0;
+
+    xcb_connection_t        *p_x_conn   = xcb_connect( NULL, &i_x_screen );
+    const xcb_setup_t       *p_x_setup  = xcb_get_setup( p_x_conn );
+    xcb_screen_iterator_t    roots      = xcb_setup_roots_iterator( p_x_setup );
+
+    xcb_screen_t              *p_x_screen    = NULL;
+    xcb_intern_atom_reply_t   *p_intern_atom = NULL;
+    xcb_atom_t                 _net_wm_pid;
+
+    xcb_get_property_cookie_t *p_cookies     = NULL;
+    xcb_window_t              *p_windows     = NULL;
+
+    xcb_window_t               vlc_main_window;
+    int i_search_ret = -1;
+
+    msg_Dbg( (vlc_object_t*) p_this, "Looking for a VLC window on X screen %d",
+                                     i_x_screen );
+
+    for( int i = i_x_screen ; roots.rem ; --i, xcb_screen_next( &roots ) )
+    {
+        if( 0 == i )
+        {
+            p_x_screen = roots.data;
+            break;
+        }
+    }
+
+    p_intern_atom = xcb_intern_atom_reply ( p_x_conn,
+        xcb_intern_atom( p_x_conn, 0,
+                         strlen( _NET_WM_PID_S ),
+                         _NET_WM_PID_S ), NULL );
+    if( !p_intern_atom )
+        return DBUS_HANDLER_RESULT_NEED_MEMORY;
+
+    _net_wm_pid = p_intern_atom->atom;
     free( p_intern_atom );
 
     xcb_query_tree_reply_t *p_tree;
@@ -230,56 +357,85 @@ DBUS_METHOD( Raise )
     if( !p_tree )
         return DBUS_HANDLER_RESULT_NEED_MEMORY;
 
-    for( int i = 0; i < xcb_query_tree_children_length( p_tree ); i++ )
-    {
-        size_t i_len = 0, i_offset = (size_t) i * sizeof( xcb_window_t );
-        char* psz_wm_window_role = NULL;
-        xcb_window_t window;
-        xcb_get_property_cookie_t cookie;
-        xcb_get_property_reply_t *p_reply;
+    i_tree_len = xcb_query_tree_children_length( p_tree );
+    p_windows  = xcb_query_tree_children( p_tree );
+    p_cookies  = calloc( i_tree_len, sizeof( xcb_get_property_cookie_t ) );
 
-        window  = *(xcb_query_tree_children( p_tree ) + i_offset);
-        cookie  = xcb_get_text_property( p_x_conn, window, wm_window_role );
-        p_reply = xcb_get_property_reply( p_x_conn, cookie, NULL );
+    for( int i = 0; i < i_tree_len; i++ )
+    {
+        p_cookies[i] = xcb_get_any_property( p_x_conn,
+                                             0,
+                                             p_windows[i],
+                                             _net_wm_pid,
+                                             INT_MAX );
+    }
+
+    for( int i = 0; i < i_tree_len; i++ )
+    {
+        int *pi_wm_pid;
+        size_t i_len = 0;
+
+        xcb_get_property_reply_t *p_reply;
+        xcb_generic_error_t      *p_x_error;
+
+        p_reply = xcb_get_property_reply( p_x_conn, p_cookies[i], &p_x_error );
 
         if( !p_reply ||
              0 == ( i_len = xcb_get_property_value_length( p_reply ) ) )
         {
+            if( !p_reply )
+            {
+                msg_Err( (vlc_object_t*) p_this,
+                         "No reply: iteration: %d Error code %d",
+                         i, p_x_error->error_code );
+
+                free( p_x_error );
+            }
+
             free( p_reply );
             p_reply = NULL;
             continue;
         }
 
-        psz_wm_window_role = calloc( i_len + 1, sizeof( char ) );
-        strncpy( psz_wm_window_role, xcb_get_property_value( p_reply ), i_len );
+        pi_wm_pid = (int*) xcb_get_property_value( p_reply );
         free( p_reply );
 
-        if( !psz_wm_window_role ||
-             strncmp( psz_wm_window_role, VLC_MAIN_WINDOW_ROLE, i_len ) )
-        {
-            free( psz_wm_window_role );
-            psz_wm_window_role = NULL;
+        if( getpid() != *pi_wm_pid )
             continue;
-        }
 
         msg_Dbg( (vlc_object_t*) p_this,
-                 "Found a VLC main window, raising it" );
+                 "Found a window which belongs to this vlc instance. "
+                 "Recursively trying to find the main VLC window" );
+
+        if( VLC_SUCCESS ==
+          ( i_search_ret = find_vlc_main_window( p_this,
+                                                 p_x_conn,
+                                                 p_windows[i],
+                                                 &vlc_main_window ) ) )
+            break;
+    }
+
+    if( VLC_SUCCESS == i_search_ret )
+    {
+        msg_Dbg( (vlc_object_t*) p_this, "VICTORY !!!!!" );
 
         /* XXX */
         const uint32_t p_stack_mode[] = { XCB_STACK_MODE_ABOVE };
-        xcb_map_window( p_x_conn, window );
-        xcb_configure_window( p_x_conn, window,
+        xcb_map_window( p_x_conn, vlc_main_window );
+        xcb_configure_window( p_x_conn, vlc_main_window,
                               XCB_CONFIG_WINDOW_STACK_MODE,
                               p_stack_mode );
-
-        free( psz_wm_window_role );
-        psz_wm_window_role = NULL;
     }
+    else
+        msg_Err( (vlc_object_t*) p_this,
+                 "Can not find a main VLC window, giving up :(" );
 
     free( p_tree );
+    free( p_cookies );
     REPLY_SEND;
 }
 
+#undef _NET_WM_PID_S
 #undef WM_WINDOW_ROLE_S
 #undef VLC_MAIN_WINDOW_ROLE
 
